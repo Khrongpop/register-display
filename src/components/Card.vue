c2fa886cd4782f73e6399cd57d1ad7bfc4d33eed
Khrongpop <mr.khrongpop@gmail.com>
2019-06-13 22:11:24 +0700

    new layout

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,69 +1,68 @@
 <template>
   <div>
-        <div class="s-card text-white" :style="`height:${windowHeight}px;`">
-        <div class="s-top clearfix">
-          <div class="float-left" style="margin-left:15px; margin-top:20px;">
+    <div class="s-card text-white">
+      <div class="s-top clearfix">
+        <div class="float-left" style="margin-left:15px; margin-top:20px;">
           <h5>Visitor.</h5>
           <h2>{{index+1}}</h2>
-          </div>
-          <div class="float-right">
-            <img src="/logo.png" width="100" style="margin-left:-150px;">
-          </div>
         </div>
-        <div class="s-img">
-          <img :src="user.image" :height="windowHeight" width="100%">
-        </div>
-        <div class="s-bottom pt-4 text-center">
-          <h1>{{user.name}}</h1>
+        <div class="float-right">
+          <img src="/logo.png" width="100" style="margin-left:-150px;">
         </div>
       </div>
+      <div class="s-img">
+        <img :src="user.image" :height="windowHeight" width="100%" v-if="size == `x`">
+        <img :src="user.image" :height="windowHeight/2" width="100%" v-else>
+      </div>
+      <div class="s-bottom pt-4 text-center">
+        <h1>{{user.name}}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props:['index','user']
-}
+  props: ["index", "user", "size"]
+};
 </script>
 
 <style lang="scss">
-@import 'node_modules/bootstrap/scss/bootstrap';
-@import 'node_modules/bootstrap-vue/src/index.scss';
+@import "node_modules/bootstrap/scss/bootstrap";
+@import "node_modules/bootstrap-vue/src/index.scss";
 
 img {
   object-fit: cover;
 }
 
 .s-top {
-  background: url('/bg-top.png') no-repeat;
+  background: url("/bg-top.png") no-repeat;
   background-size: contain;
   height: 400px;
-    width: 100%;
-    position: absolute;
-    z-index: 999;
-    padding: 15px;
+  width: 100%;
+  position: absolute;
+  z-index: 999;
+  padding: 15px;
 }
 
 .s-bottom {
-  background: url('/bg-bot.png') no-repeat;
+  background: url("/bg-bot.png") no-repeat;
   background-size: contain;
   height: 400px;
   width: 100%;
   bottom: -184px;
-  @include media-breakpoint-up(xl) { 
-      bottom: -200px;
-
-   }
+  @include media-breakpoint-up(xl) {
+    bottom: -200px;
+  }
 
   position: absolute;
   z-index: 999;
   padding: 15px;
   h1 {
     font-size: 60px;
-    display: inline-block;    
+    display: inline-block;
     // background: url('/bg-text.png') no-repeat;
-      background-size: contain; 
+    background-size: contain;
   }
 }
-
 </style>
--- src/views/Display1.vue
+++ src/views/Display1.vue
@@ -1,32 +1,49 @@
 <template>
   <div class="home">
-    <div class="row">
-
-    <div v-for="(user, index) in users" :key="user.name" class="col-4 p-0" style="height:100%;">
+    <div style="width:100%; float:left height:100%; ">
+      <!-- <div v-for="(user, index) in users" :key="user.name" class="col-4 p-0" style="height:100%;">
         <card :user="user" :index="index" v-if="index == 0"/>
 
         <div v-if="index > 0">
           <card :user="user" :index="index"/>
         </div>
-    </div>
-      </div>
+      </div>-->
 
+      <div v-for="(user, index) in users" :key="user.name">
+        <div v-if="index === 0" style="width:50%; height:100%; float:left">
+          <card :user="user" :index="index" size="x"/>
+        </div>
+        <!-- <div v-else>
+          <div style="width:100%; float:left">
+            <card :user="user" :index="index"/>
+          </div>
+        </div>-->
+      </div>
+      <div style="width:50%; height:100%; float:right">
+        <div v-for="(user, index) in users" :key="user.name">
+          <div v-if="index > 0 && index < 3">
+            <div style="width:100%;  height:50%; float:left">
+              <card :user="user" :index="index" size="y"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Card from '../components/Card';
-import { usersRef } from '@/firebaseConfig.js'
+import Card from "../components/Card";
+import { usersRef } from "@/firebaseConfig.js";
 export default {
-  firebase:{
+  firebase: {
     users: usersRef
   },
-  components:{
+  components: {
     Card
   },
-  name: 'home'
-}
+  name: "home"
+};
 </script>
 <style>
-
 </style>
