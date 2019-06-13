8d498b0cd71513cb176ab158d9f9bce3eb493545
Khrongpop <mr.khrongpop@gmail.com>
2019-06-13 22:59:57 +0700

    bg

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,7 +10,8 @@
           <img src="/logo.png" width="100" style="margin-left:-150px;">
         </div>
       </div>
-      <div v-else class="bg2 clearfix h-500"></div>
+      <div v-else class="bg2 clearfix h-600"></div>
+      <div v-if="size == `y`" class="bg3 clearfix h-600"></div>
       <div class="s-img">
         <img :src="user.image" :height="windowHeight" width="100%" v-if="size == `x`">
         <img :src="user.image" :height="windowHeight/2" width="100%" v-else>
@@ -46,12 +47,20 @@ img {
 }
 .nun {
   position: relative;
+  overflow: hidden;
 }
 .bg2 {
-  background: url("/bg2.png") no-repeat;
+  background: url("/left-bg.png") no-repeat;
   background-size: contain;
   width: 100%;
   position: absolute;
+}
+.bg3 {
+  background: url("/right-bg.png") no-repeat;
+  background-size: contain;
+  width: 100%;
+  position: absolute;
+  left: 450px;
 }
 .s-top {
   background: url("/bg-top.png") no-repeat;
@@ -60,6 +69,10 @@ img {
   position: absolute;
   z-index: 999;
   padding: 15px;
+}
+.h-600 {
+  margin-top: 0;
+  height: 600px;
 }
 .h-500 {
   margin-top: 0;
