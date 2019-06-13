ff9ca806b89cbc8dc6dc1c13a43318884b09d308
Khrongpop <mr.khrongpop@gmail.com>
2019-06-14 04:19:38 +0700

    function

--- src/views/Display1.vue
+++ src/views/Display1.vue
@@ -11,7 +11,8 @@
 
       <div v-for="(user, index) in users" :key="user.name">
         <div v-if="index === 0" style="width:50%; height:100%; float:left">
-          <card :user="user" :index="index" size="x"/>
+          <!-- <card :user="user" :index="index" size="x"/> -->
+          <card :user="users[getFistIndex]" :index="index" size="x"/>
         </div>
         <!-- <div v-else>
           <div style="width:100%; float:left">
@@ -21,7 +22,18 @@
       </div>
       <div style="width:50%; height:100%; float:right">
         <div v-for="(user, index) in users" :key="user.name">
-          <div v-if="index > 0 && index < 3">
+          <!-- <div v-if="index > 0 && index < 3">
+            <div style="width:100%;  height:50%; float:left">
+              <card :user="user" :index="index" size="y"/>
+            </div>
+          </div>-->
+          <div v-if="index == 1">
+            <div style="width:100%;  height:50%; float:left">
+              <card :user="user" :index="index" size="y"/>
+            </div>
+          </div>
+
+          <div v-else-if="index == 2">
             <div style="width:100%;  height:50%; float:left">
               <card :user="user" :index="index" size="y"/>
             </div>
@@ -41,6 +53,59 @@ export default {
   },
   components: {
     Card
+  },
+  computed: {
+    getFistIndex() {
+      let count = this.users.length;
+      if (count <= 1) {
+        return count - 1;
+      } else {
+        if (count % 3 == 0) {
+          console.log(count % 3);
+          return count - 2 - 1;
+        } else if (count % 3 == 2) {
+          console.log(count % 2);
+          return count - 1 - 1;
+        } else {
+          console.log(count % 1);
+          return count - 1;
+        }
+      }
+    },
+    getSecondIndex() {
+      let count = this.users.length;
+      if (count <= 1) {
+        return count - 1;
+      } else {
+        if (count % 3 == 0) {
+          console.log(count % 3);
+          return count - 1 - 1;
+        } else if (count % 3 == 2) {
+          console.log(count % 2);
+          return count - 1;
+        } else {
+          console.log(count % 1);
+          return count - 2 - 1;
+        }
+      }
+    },
+    getThirdIndex() {
+      let count = this.users.length;
+      if (count <= 2) {
+        return count - 1;
+      } else {
+        if (count % 3 == 0) {
+          console.log(count % 3);
+          return count - 1;
+        } else if (count % 3 == 2) {
+          console.log(count % 2);
+          return count - 2 - 1;
+        } else {
+          console.log(count % 1);
+          return count - 1 - 1;
+        }
+      }
+    }
   },
   name: "home"
 };
--- src/views/Display2.vue
+++ src/views/Display2.vue
@@ -1,19 +1,330 @@
 <template>
   <div class="home">
-    <h1>display2</h1>
-    <pre>
-    {{users}}
-    </pre>
+    {{get5Index}}
+    <!-- <div style="width:50%; height:100%; float:left">
+      <card :user="users[getFistIndex]" :index="index" size="x"/>
+    </div>
+    <div style="width:50%; height:100%; float:right">
+      <div style="width:100%;  height:50%; float:left">
+        <card :user="users[getSecondIndex]" :index="index" size="y"/>
+      </div>
+      <div style="width:100%;  height:50%; float:left">
+        <card :user="users[getThirdIndex]" :index="index" size="y"/>
+      </div>
+    </div>-->
+    <input type="text" v-model="count">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import { usersRef } from '@/firebaseConfig.js'
+import Card from "../components/Card";
+import { usersRef } from "@/firebaseConfig.js";
 export default {
-  firebase:{
+  firebase: {
     users: usersRef
   },
-  name: 'home',
-}
+  data() {
+    return {
+      count: 0
+    };
+  },
+  components: {
+    Card
+  },
+  computed: {
+    getFistIndex() {
+      // let count = this.users.length;
+      let count = this.count;
+      let result = count;
+      console.log(count % 9);
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 1;
+        } else {
+          var i;
+          for (i = 0; i <= count; i++) {
+            if (count % 9 === i) {
+              result = count - i;
+            }
+          }
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 0) {
+        return result - 1;
+      }
+      return result;
+    },
+    getSecondIndex() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 8;
+        } else if (count % 9 == 1) {
+          result = count - 1;
+        } else if (count % 9 == 2) {
+          result = count - 1;
+        } else if (count % 9 == 3) {
+          result = count - 2;
+        } else if (count % 9 == 4) {
+          result = count - 3;
+        } else if (count % 9 == 5) {
+          result = count - 4;
+        } else if (count % 9 == 6) {
+          result = count - 5;
+        } else if (count % 9 == 7) {
+          result = count - 6;
+        } else if (count % 9 == 8) {
+          result = count - 7;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 1) {
+        return result - 1;
+      }
+      return result;
+    },
+    getThirdIndex() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 7;
+        } else if (count % 9 == 1) {
+          result = count - 8;
+        } else if (count % 9 == 2) {
+          result = count - 1;
+        } else if (count % 9 == 3) {
+          result = count - 1;
+        } else if (count % 9 == 4) {
+          result = count - 2;
+        } else if (count % 9 == 5) {
+          result = count - 3;
+        } else if (count % 9 == 6) {
+          result = count - 4;
+        } else if (count % 9 == 7) {
+          result = count - 5;
+        } else if (count % 9 == 8) {
+          result = count - 6;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 2) {
+        return result - 1;
+      }
+      return result;
+    },
+    get4Index() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 6;
+        } else if (count % 9 == 1) {
+          result = count - 7;
+        } else if (count % 9 == 2) {
+          result = count - 8;
+        } else if (count % 9 == 3) {
+          result = count - 1;
+        } else if (count % 9 == 4) {
+          result = count - 1;
+        } else if (count % 9 == 5) {
+          result = count - 2;
+        } else if (count % 9 == 6) {
+          result = count - 3;
+        } else if (count % 9 == 7) {
+          result = count - 4;
+        } else if (count % 9 == 8) {
+          result = count - 5;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 3) {
+        return result - 1;
+      }
+      return result;
+    },
+    get5Index() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 5;
+        } else if (count % 9 == 1) {
+          result = count - 6;
+        } else if (count % 9 == 2) {
+          result = count - 7;
+        } else if (count % 9 == 3) {
+          result = count - 8;
+        } else if (count % 9 == 4) {
+          result = count - 1;
+        } else if (count % 9 == 5) {
+          result = count - 1;
+        } else if (count % 9 == 6) {
+          result = count - 2;
+        } else if (count % 9 == 7) {
+          result = count - 3;
+        } else if (count % 9 == 8) {
+          result = count - 4;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 4) {
+        return result - 1;
+      }
+      return result;
+    },
+    get6Index() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 4;
+        } else if (count % 9 == 1) {
+          result = count - 5;
+        } else if (count % 9 == 2) {
+          result = count - 6;
+        } else if (count % 9 == 3) {
+          result = count - 7;
+        } else if (count % 9 == 4) {
+          result = count - 8;
+        } else if (count % 9 == 5) {
+          result = count - 1;
+        } else if (count % 9 == 6) {
+          result = count - 1;
+        } else if (count % 9 == 7) {
+          result = count - 2;
+        } else if (count % 9 == 8) {
+          result = count - 3;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 5) {
+        return result - 1;
+      }
+      return result;
+    },
+    get7Index() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 3;
+        } else if (count % 9 == 1) {
+          result = count - 4;
+        } else if (count % 9 == 2) {
+          result = count - 5;
+        } else if (count % 9 == 3) {
+          result = count - 6;
+        } else if (count % 9 == 4) {
+          result = count - 7;
+        } else if (count % 9 == 5) {
+          result = count - 8;
+        } else if (count % 9 == 6) {
+          result = count - 1;
+        } else if (count % 9 == 7) {
+          result = count - 1;
+        } else if (count % 9 == 8) {
+          result = count - 2;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 6) {
+        return result - 1;
+      }
+      return result;
+    },
+    get7Index() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 2;
+        } else if (count % 9 == 1) {
+          result = count - 3;
+        } else if (count % 9 == 2) {
+          result = count - 4;
+        } else if (count % 9 == 3) {
+          result = count - 5;
+        } else if (count % 9 == 4) {
+          result = count - 6;
+        } else if (count % 9 == 5) {
+          result = count - 7;
+        } else if (count % 9 == 6) {
+          result = count - 8;
+        } else if (count % 9 == 7) {
+          result = count - 1;
+        } else if (count % 9 == 8) {
+          result = count - 1;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 7) {
+        return result - 1;
+      }
+      return result;
+    },
+    get8Index() {
+      let count = this.count;
+      let result = count;
+      if (count <= 1) {
+        result = count - 1;
+      } else {
+        if (count % 9 == 0) {
+          result = count - 1;
+        } else if (count % 9 == 1) {
+          result = count - 2;
+        } else if (count % 9 == 2) {
+          result = count - 3;
+        } else if (count % 9 == 3) {
+          result = count - 4;
+        } else if (count % 9 == 4) {
+          result = count - 5;
+        } else if (count % 9 == 5) {
+          result = count - 6;
+        } else if (count % 9 == 6) {
+          result = count - 7;
+        } else if (count % 9 == 7) {
+          result = count - 8;
+        } else if (count % 9 == 8) {
+          result = count - 1;
+        }
+      }
+      if (result < 9) {
+        return result;
+      } else if (result % 9 === 8) {
+        return result - 1;
+      }
+      return result;
+    }
+  },
+  name: "home"
+};
 </script>
+<style>
+</style>
